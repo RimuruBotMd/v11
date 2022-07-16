9b6b8cd60df1bba1f4b03b68ff67fac8abc2d528
RimuruBotMd <108895247+RimuruBotMd@users.noreply.github.com>
2022-07-16 23:25:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -56,8 +56,8 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
+global.packname = Siesta-Md
+global.author = SabilOfc
 global.wm = '© Siesta'
 
 global.multiplier = 1000 // The higher, The harder levelup
